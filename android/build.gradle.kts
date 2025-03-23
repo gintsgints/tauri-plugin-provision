291d1046cd33a24ae886be77f26972f35280bc38
Gints Polis <polis.gints@gmail.com>
2025-03-23 09:39:27 +0200

    Required dependencies for provisioning
    required

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -41,5 +41,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("com.fasterxml.jackson.core:jackson-core:2.15.3")
+    androidTestImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
     implementation(project(":tauri-android"))
 }
