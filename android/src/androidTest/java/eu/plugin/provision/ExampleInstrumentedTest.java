aa7d015abbfd8bf5ca7e282c44fe285d3800b747
Gints Polis <polis.gints@gmail.com>
2025-03-23 09:38:53 +0200

    Try Instrumented test (it hangs)

---
+++ android/src/androidTest/java/eu/plugin/provision/ExampleInstrumentedTest.java
@@ -0,0 +1,44 @@
+package eu.plugin.provision;
+
+import static org.junit.Assert.assertEquals;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import app.tauri.plugin.Invoke;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<Activity> activityRule = new ActivityScenarioRule<>(Activity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("eu.plugin.provision.test", appContext.getPackageName());
+    }
+
+    @Test
+    public void testScan() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Activity activity = new Activity();
+        ProvisionClientPlugin provisionClientPlugin = new ProvisionClientPlugin(activity);
+        Invoke invoke = new Invoke(1, "test", 1, 1, null, "", null);
+        provisionClientPlugin.startScan(invoke);
+    }
+}
